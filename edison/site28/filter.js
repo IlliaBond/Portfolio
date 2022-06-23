87c978e50ce2af39fde8b37166ad71ebc8d29808
IlliaBond <illiabond523@gmail.com>
2022-06-23 16:32:11 +0300

    Portfolio edit 1

--- edison/site28/filter.js
+++ edison/site28/filter.js
@@ -1,21 +1,20 @@
-const liItem = document.querySelectorAll('.filter-menu li');
-const imgItem = document.querySelectorAll('.product .itemBox');
-
-liItem.forEach(li => {
+const liItame = document.querySelectorAll('.filter-menu li');
+const imgItame = document.querySelectorAll('.product .itemBox');
+liItame.forEach(li =>{
     li.onclick = () =>{
-        liItem.forEach(other => {
+        liItame.forEach(other =>{
             other.className = "";
-        })
+        });
         li.className = "active";
-
         const value = li.textContent;
-        imgItem.forEach(img => {
+        imgItame.forEach(img =>{
             img.classList.add("delete");
             img.classList.remove("active");
-            if(img.getAttribute('data-item') == value.toLowerCase() || value == "All"){
-                img.classList.add("active");
-                img.classList.remove("delete");
-            }
-        })
+       
+       if(img.getAttribute('data-item') == value.toLowerCase() || value =="All"){
+           img.classList.remove("delete");
+           img.classList.add("active");
+       }
+    });
     }
 })
